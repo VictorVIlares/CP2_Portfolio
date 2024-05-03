168be6616b95a734e5fccdf00a60c5f59512abba
Vilares <77464500+ULTTRA7@users.noreply.github.com>
2024-05-03 20:12:18 -0300

    Quase acabando

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,17 @@
+import About from "./Componentes/About";
 import Banner from "./Componentes/Banner";
 import Nav from "./Componentes/Nav";
+import Skills from "./Componentes/Skills";
+import Projects from "./Componentes/Projects";
 
 const App = () => {
   return (
       <div className="bg-slate-900">
         <Nav/>
         <Banner/>
+        <About/>
+        <Skills/>
+        <Projects/>
       </div>
   );
 };
--- portfolio/src/Componentes/Banner.jsx
+++ portfolio/src/Componentes/Banner.jsx
@@ -2,13 +2,23 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 import { FiTwitter } from 'react-icons/fi';
 import img from '../assets/profile.jpeg'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 const Banner = () => {
+    useEffect( () =>{
+    AOS.init({
+        easing: "ease-out-quart",
+        delay: 0,
+        duration: 750
+    })
+},[])
     return (
-        <div className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
+        <div id="Home" data-aos="fade-up" className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
             <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
-                <h1 className="text-[52px] font-semibold mb-8 leading-normal">Olá! Meu nome é<span className="text-cyan-400"> Larissa Estella</span></h1>
-                <h1 className="text-[20px] font-semibold mb-8 leading-normal">Estudante de Engenharia de Sotfware na FIAP</h1>
+                <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal uppercase">Olá! Meu nome é<span className="text-cyan-400"> Larissa Estella</span></h1>
+                <h1 data-aos="fade-right" className="text-[20px] font-semibold mb-8 leading-normal">Estudante de Engenharia de Sotfware na FIAP</h1>
                 <div className="flex mt-8 gap-2">
                     <div className="flex items-center justify-center">
                         <div className="flex space-x-2">
@@ -28,7 +38,7 @@ const Banner = () => {
                     </div>
                 </div>
             </div>
-            <img src={img} width={290} height={290} className='rounded-full border-2 p-1 border-cyan-400 img_glow' alt="imagem" />
+            <img data-aos="fade-up"src={img} width={290} height={290} className='rounded-full border-2 p-1 border-cyan-400 img_glow' alt="imagem" />
         </div>
     );
 };
