b1a290da96edab357b80433025104707d01e358c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-03 12:07:34 -0300

    projeto

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Componentes/Banner";
+import Nav from "./Componentes/Nav";
+
+const App = () => {
+  return (
+      <div className="bg-slate-900">
+        <Nav/>
+        <Banner/>
+      </div>
+  );
+};
+
+export default App;
